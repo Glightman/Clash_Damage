976989249951693cf926d4dce85adf5978b63b1b
Gabriel Santana <gabriellima36716@gmail.com>
2021-06-18 23:23:57 -0300

    inserindo as armas

--- static/index.js
+++ static/index.js
@@ -1,5 +1,5 @@
 
-const titulo = document.getElementById('titulo');
+/* const titulo = document.getElementById('titulo'); */
 const botaoRei = document.getElementById('rei');
 const botaoGolem = document.getElementById('golem');
 const botaoArcher = document.getElementById('archer');
@@ -9,31 +9,23 @@ var img = document.querySelector("#imagem");
 
 function abrirDiv(){
   document.getElementById('box_pop').style.display = 'block';
- /*  setTimeout(2000)
-  document.getElementById('box_pop').style.opacity = 0.5;
-  setTimeout(2000)
-  document.getElementById('box_pop').style.opacity = 1; */
 }
 
 function fecharDiv(){
   document.getElementById('box_pop').style.display = 'none';
-/*   document.getElementById('box_pop').style.opacity = 0; */
 }
 
 botaoRei.addEventListener('click', () => {
-  titulo.innerHTML = 'Rei Bárbaro';
   img.setAttribute('src', '../static/rei_barbaro.png');
   abrirDiv()
 })
 
 botaoGolem .addEventListener('click', () => {
-  titulo.innerHTML = 'Golem';
   img.setAttribute('src', '../static/Golem.png');
   abrirDiv()
 })
 
 botaoArcher.addEventListener('click', () => {
-  titulo.innerHTML = 'Archer';
   img.setAttribute('src', '../static/Archer.png');
   abrirDiv()
 })
